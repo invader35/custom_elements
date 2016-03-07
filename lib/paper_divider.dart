017941202b5d2b50d6286f18eccef69c63cf2c5c
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-03-07 11:29:45 +0100

    added `paper-divider`

---
+++ lib/paper_divider.dart
@@ -0,0 +1,21 @@
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `paper_divider`.
+@HtmlImport('paper_divider_nodart.html')
+library custom_elements.lib.src.paper_divider.paper_divider;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+
+/// A Material Divider Element.
+///
+/// Example:
+///
+///     <paper-divider></paper-divider>
+@CustomElementProxy('paper-divider')
+class PaperDivider extends HtmlElement with CustomElementProxyMixin, PolymerBase {
+  PaperDivider.created() : super.created();
+  factory PaperDivider() => new Element.tag('paper-divider');
+}
