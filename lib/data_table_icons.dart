99c1681b1ce932305f6a366e85ff7db9ed88a611
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-07-03 16:52:17 +0200

    missing icons

---
+++ lib/data_table_icons.dart
@@ -0,0 +1,11 @@
+// DO NOT EDIT: auto-generated with `pub run custom_element_apigen:update`
+
+/// Dart API for the polymer element `data_table_icons`.
+@HtmlImport('data_table_icons_nodart.html')
+library custom_elements.lib.src.iron_data_table.data_table_icons;
+
+import 'dart:html';
+import 'dart:js' show JsArray, JsObject;
+import 'package:web_components/web_components.dart';
+import 'package:polymer_interop/polymer_interop.dart';
+import 'package:polymer_elements/iron_iconset_svg.dart';
