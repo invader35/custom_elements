a42f61ea8a20fbd944dc236d8aeafbf53ed5a734
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-09-07 18:38:39 +0200

    old issue with animations

--- demo/web/index.dart
+++ demo/web/index.dart
@@ -4,6 +4,7 @@ import "dart:html";
 import "package:demo_custom_elements/demo.dart";
 
 import "package:logging/logging.dart";
+import 'package:polymer_interop/polymer_interop.dart';
 
 main() async {
 
@@ -12,5 +13,5 @@ main() async {
     print('${rec.level.name}: ${rec.time}: ${rec.loggerName} - ${rec.message}');
   });
 
-  await initPolymer();
+  await initPolymer(strategy:checkForEs6ProxySupport());
 }
